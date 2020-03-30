0201ccd9c04ae38977b4ff67a9805e283a39593a
wangbiao <wangbiao1012@gmail.com>
2020-03-30 08:46:11 +0800

    netty-http方式

--- netty-http/src/main/java/com/sanshengshui/netty/HttpHelloWorldServerHandler.java
+++ netty-http/src/main/java/com/sanshengshui/netty/HttpHelloWorldServerHandler.java
@@ -29,7 +29,7 @@ import static io.netty.handler.codec.http.HttpResponseStatus.OK;
 import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
 /**
- * @author james
+ * @author
  */
 public class HttpHelloWorldServerHandler extends SimpleChannelInboundHandler<HttpObject> {
 
